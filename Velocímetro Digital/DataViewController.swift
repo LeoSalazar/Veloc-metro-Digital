e374ad897e94c837ce69d630368b4aecbde1f79d
LeoSalazar <Leonardo@leonardos-mbp.lan>
2016-03-19 22:08:24 -0600

    Initial Commit

---
+++ Velocímetro Digital/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Velocímetro Digital
+//
+//  Created by Leonardo Salazar Flores on 19/03/16.
+//  Copyright © 2016 Leonardo Salazar Flores. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
